ed56c444c4a99ad08b1727ca69b2c557951ddbc1
Manuel Ramos <manuel.ramos@vodafone.com>
2024-03-28 12:56:41 +0000

    Initial commit

---
+++ src/test/java/com/vodafone/deal/SportyShoesPrototype/SportyShoesPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.deal.SportyShoesPrototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
